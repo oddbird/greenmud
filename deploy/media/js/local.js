ae0e018e05f2536b4261034f31e920d4f1a547a1
Jonny Gerig Meyer <jonny@oddbird.net>
2012-01-17 17:23:24 -0500

    basic js bookmark/history working

--- deploy/media/js/local.js
+++ deploy/media/js/local.js
@@ -1,10 +1,13 @@
 /*jslint    browser:    true,
-            indent:     4 */
-/*global    jQuery */
+            indent:     4,
+            confusion:  true */
+/*global    jQuery, History */
 
 var GM = (function (GM, $) {
 
     'use strict';
+
+    var History = window.History;
 
     // Store keycode variables for easier readability
     GM.keycodes = {
@@ -25,14 +28,28 @@ var GM = (function (GM, $) {
 
     GM.toggleControls = function (toggle, controls) {
         var doToggle = function () {
+            var controlsIndex = window.location.search.indexOf('&controls='),
+                page;
+            if (controlsIndex === -1) {
+                page = window.location.search.substring(1);
+            } else {
+                page = window.location.search.substring(1, controlsIndex);
+            }
             $(controls).toggleClass('active');
             $(toggle).toggleClass('active');
-            return false;
+            if ($('#toggle').hasClass('active')) {
+                History.replaceState(null, null, '?' + page + '&controls=true');
+            } else {
+                History.replaceState(null, null, '?' + page);
+            }
         };
 
-        $(toggle).click(doToggle);
+        $(toggle).click(function (e) {
+            doToggle();
+            e.preventDefault();
+        });
 
-        if (window.location.search === '?controls') {
+        if (window.location.search.lastIndexOf('&controls=true') === window.location.search.length - 14) {
             doToggle();
         }
     };
@@ -53,11 +70,21 @@ var GM = (function (GM, $) {
         var prev = $('.controls .pagenav .prev a'),
             next = $('.controls .pagenav .next a'),
             pages = $('#main .page'),
-            turnToPage = function (element) {
+            turnToPage = function (element, stateChange) {
+                var id = element.attr('id'),
+                    controls = '';
+                if ($('#toggle').hasClass('active')) {
+                    controls = '&controls=true';
+                }
                 pages.removeClass('active-page prev-page next-page');
                 element.addClass('active-page');
                 element.next('.page').addClass('next-page');
                 element.prev('.page').addClass('prev-page');
+                if (stateChange) {
+                    History.pushState(null, null, '?' + id + controls);
+                } else {
+                    History.replaceState(null, null, '?' + id + controls);
+                }
 
                 prev.add(next).removeClass('active');
                 if (pages.filter('.prev-page').length) {
@@ -68,15 +95,15 @@ var GM = (function (GM, $) {
                 }
             };
 
-        turnToPage(pages.first());
-
         prev.click(function (e) {
             var href = $(this).attr('href');
             if (pages.filter('.prev-page').length) {
-                turnToPage(pages.filter('.prev-page'));
+                turnToPage(pages.filter('.prev-page'), true);
             } else if (href !== undefined) {
                 if ($('#toggle').hasClass('active')) {
-                    href = href + '?controls';
+                    href = href + '?last&controls=true';
+                } else {
+                    href = href + '?last';
                 }
                 window.location = href;
             }
@@ -86,10 +113,10 @@ var GM = (function (GM, $) {
         next.click(function (e) {
             var href = $(this).attr('href');
             if (pages.filter('.next-page').length) {
-                turnToPage(pages.filter('.next-page'));
+                turnToPage(pages.filter('.next-page'), true);
             } else if (href !== undefined) {
                 if ($('#toggle').hasClass('active')) {
-                    href = href + '?controls';
+                    href = href + '?&controls=true';
                 }
                 window.location = href;
             }
@@ -106,6 +133,25 @@ var GM = (function (GM, $) {
                 next.first().click();
             }
         });
+
+        $(window).on('statechange', function (e) {
+            var controls = window.location.search.indexOf('&controls='),
+                page;
+            if (controls === -1) {
+                page = window.location.search.substring(1);
+            } else {
+                page = window.location.search.substring(1, controls);
+            }
+            if (page === 'last') {
+                turnToPage(pages.last());
+            } else if (page) {
+                turnToPage(pages.filter('#' + page));
+            } else {
+                turnToPage(pages.first());
+            }
+        });
+
+        $(window).trigger('statechange');
     };
 
     return GM;
