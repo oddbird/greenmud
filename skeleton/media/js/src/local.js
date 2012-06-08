1961077a44a1ca85e3e264f43e4dd697de021499
Carl Meyer <carl@oddbird.net>
2012-06-08 13:43:11 -0600

    Added content skeleton.

---
+++ skeleton/media/js/src/local.js
@@ -0,0 +1,52 @@
+var GM = (function (GM, $) {
+
+    'use strict';
+
+    // Store keycode variables for easier readability
+    GM.keycodes = {
+        SPACE: 32,
+        ENTER: 13,
+        TAB: 9,
+        ESC: 27,
+        BACKSPACE: 8,
+        SHIFT: 16,
+        CTRL: 17,
+        ALT: 18,
+        CAPS: 20,
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39,
+        DOWN: 40
+    };
+
+    GM.toggleControls = function (toggle, controls, hash) {
+        var doToggle = function () {
+            $(controls).toggleClass('active');
+            $(toggle).toggleClass('active');
+        };
+
+        $(toggle).click(function (e) {
+            doToggle();
+            return false;
+        });
+
+        if (hash && window.location.hash === '#' + hash) {
+            doToggle();
+        }
+    };
+
+    GM.bodyHeight = function (element, property) {
+        var updateHeight = function (target) {
+            $(target).css(property, $(window).height());
+        };
+        updateHeight(element);
+        $(window).resize(function () {
+            $.doTimeout('resize', 250, function () {
+                updateHeight(element);
+            });
+        });
+    };
+
+    return GM;
+
+}(GM || {}, jQuery));
