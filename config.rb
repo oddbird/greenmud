421000feef3b3b423e44327bb93f2c23991143ed
Eric Meyer <eric@oddbird.net>
2012-06-08 13:52:54 -0600

    Update compass config to use skeleton

--- config.rb
+++ config.rb
@@ -9,10 +9,10 @@ project_type = :stand_alone
 # Set this to the root of your project when deployed:
 http_path = "/"
 sass_dir = "sass"
-css_dir = "content/media/css"
-images_dir = "content/media/images"
-fonts_dir = "content/media/fonts"
-javascripts_dir = "content/media/js"
+css_dir = "skeleton/media/css"
+images_dir = "skeleton/media/images"
+fonts_dir = "skeleton/media/fonts"
+javascripts_dir = "skeleton/media/js"
 line_comments = false
 preferred_syntax = :sass
 output_style = :compact
