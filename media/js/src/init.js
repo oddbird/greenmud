e471fc4bc9b8773c8385870912de2a7ec0c528a2
Eric Meyer <eric@oddbird.net>
2012-06-13 01:34:29 -0600

    Out with the old, in with the new

---
+++ media/js/src/init.js
@@ -0,0 +1,20 @@
+var GM = (function (GM, $) {
+
+    'use strict';
+
+    $(function () {
+        // plugins
+        $('input[placeholder], textarea[placeholder]').placeholder();
+
+        // local.js
+        GM.toggleControls('#toggle', '.controls', 'controls');
+        GM.toggleControls('a[rel="contents"]', '#toc');
+        GM.bodyHeight('.page', 'min-height');
+
+        // pageturning.js
+        GM.pageTurning();
+    });
+
+    return GM;
+
+}(GM || {}, jQuery));
